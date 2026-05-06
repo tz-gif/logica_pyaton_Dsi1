16d7f6c99a88f77745d510ad67e6faf560da1b49
tz-gif <davimrodrigues3@gmail.com>
2026-05-06 11:25:42 -0300

    meu primeiro programa python

---
+++ 1.1-numeros-em-python.py
@@ -0,0 +1,35 @@
+# aula completa: numeros em python.py
+
+""""
+vamos aprender:
+1- tipos de numeros 
+2-conversoes de tipos
+3-hierarquia numerica
+4-operaçoes matematicas
+5-coerção de tipos
+6-verificação de tipos
+7-entrada de dados
+"""
+###########################
+##PASSO 01 - TIPOS NUMERICOS 
+###########################
+# INT-> NUMEROS INTEROS 
+# FLOAT-> NUMEROS COM CASAS DECMAIS 
+# COMPLEX -> NUMEROS COMPLEXOS (USADO EM MATEMATICA / ENGEARIA )#
+
+print("--TIPOS DE NUMERICOS--")
+
+# EXEMPLOS 01 - NUMEROS INTEROS
+# CRIAMOS UMA VARIAVEL  CHAMADO NUMERO_INTERO
+numero_imteiro=10 
+print("valor:",numero_imteiro)
+
+# type() mostra qual é o tipo da variavel 
+print("tipo:", type(numero_imteiro)
+      
+print("----------------------------"")
+      
+
+      
+
+
